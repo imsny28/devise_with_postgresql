ca9b5ac4addb753bf7f4bcf9c53d39967ddfa07a
Fateh Singh <fateh@maropost.com>
2024-05-10 11:43:37 +0530

    microsoft sso

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,13 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :recoverable, :rememberable, :validatable,
-         :two_factor_authenticatable, otp_secret_encryption_key: Devise.secret_key
+         # :two_factor_authenticatable, otp_secret_encryption_key: Devise.secret_key,
+         :omniauthable, omniauth_providers: [:azure_activedirectory_v2]
 
-  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_01_150907) do
+ActiveRecord::Schema.define(version: 2024_05_09_154729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2024_05_01_150907) do
     t.string "encrypted_otp_secret_salt"
     t.integer "consumed_timestep"
     t.boolean "otp_required_for_login"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
