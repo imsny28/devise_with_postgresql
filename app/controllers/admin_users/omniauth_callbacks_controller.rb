b728a767b6506094a5a1197325343134ec54f4cf
Fateh Singh <fateh@maropost.com>
2024-05-15 17:43:48 +0530

    remove extra code

--- app/controllers/admin_users/omniauth_callbacks_controller.rb
+++ app/controllers/admin_users/omniauth_callbacks_controller.rb
@@ -4,12 +4,7 @@ class AdminUsers::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
     @admin_user = AdminUser.from_omniauth(auth)
 
     if @admin_user.persisted?
-      # sign_in_and_redirect(admin_root_path, event: :authentication)
-      # byebug
       sign_in_and_redirect @admin_user, event: :authentication
-
-      # sign_in(admin_user, event: :authentication)
-      # redirect_to admin_admin_users_path
     else
       redirect_to new_admin_user_session_path, alert: 'Authentication failed.'
     end
--- app/controllers/admin_users/sessions_controller.rb
+++ app/controllers/admin_users/sessions_controller.rb
@@ -3,32 +3,9 @@ module AdminUsers
     layout 'active_admin_logged_out'
     helper ::ActiveAdmin::ViewHelpers
 
-    def create
-      admin_user = AdminUser.find_by(email: sign_in_params[:email])
-      is_valid, error = authenticate(admin_user, sign_in_params[:password], sign_in_params[:otp_attempt])
-      if is_valid
-        flash.discard
-        sign_in(admin_user, event: :authentication)
-        redirect_to admin_admin_users_path
-      else
-        flash[:error] = error
-        @admin_user = AdminUser.find_by(email: sign_in_params[:email]) || AdminUser.new
-        render :new
-      end
-    end
-
     def after_sign_out_path_for(resource)
       '/admin'
     end
 
-    private
-
-    def authenticate(admin_user, password, otp_attempt)
-      return [false, "Invalid email"] unless admin_user.present?
-      return [false, "Invalid password"] unless admin_user.valid_password?(password)
-      return [true, ""] unless admin_user.otp_required_for_login
-      return [false, "Invalid authentication code"] unless admin_user.validate_and_consume_otp!(otp_attempt)
-      return [true, ""]
-    end
   end
 end
